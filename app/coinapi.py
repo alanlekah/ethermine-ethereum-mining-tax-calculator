bfbdc0bfa073e1579d10070e68a52cf4ad6eeaee
Alan Lekah <alekah@live.com>
2022-02-11 19:00:00 -0500

    updated epoch time to go out farther

--- app/coinapi.py
+++ app/coinapi.py
@@ -15,7 +15,7 @@ class CoinAPI:
                 ret = self.cg.get_coin_market_chart_range_by_id(id='ethereum',
                                                                 vs_currency='usd',
                                                                 from_timestamp=epoch,
-                                                                to_timestamp=(epoch+3600))
+                                                                to_timestamp=(epoch+5600))
                 return ret
             except requests.exceptions.HTTPError:
                 print("Sleeping..")
